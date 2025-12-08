d31188260fb61b947cae752c4dca83c81da5f95e
unknown <priyanshu.arthayukti1998@gmail.com>
2025-12-08 15:59:17 +0530

    AddeD

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
 import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { StyleSheet } from "react-native";
+import { AuthProvider } from "./context/AuthContext";
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={styles.container}>
+      <AuthProvider>
       <Stack screenOptions={{ headerShown: false }} />
+      </AuthProvider>
     </GestureHandlerRootView>
   );
 }
