6d3a3f4524d7ff9f14e9b293481dfc2815a6bcb8
Arthayukti Fintech <arthayuktifintech@Arthayuktis-Mac-mini.local>
2025-12-20 13:08:22 +0530

    profile api integrated

--- redux/config/configURL.ts
+++ redux/config/configURL.ts
@@ -1,6 +1,6 @@
 // src/redux/config/configURL.ts
 
 export const base_URL =
-    // "http://192.168.0.125:5001"
-    "http://192.168.0.180:5001"
+    "http://192.168.0.125:5001"
+    // "http://192.168.0.180:5001"
 //   : "https://server.assetlend.in";
